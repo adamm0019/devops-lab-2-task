46727aa76b00303e0f654ef71ec411b19434a92b
Adam Malone <adam-malone@hotmail.co.uk>
2025-01-30 12:55:00 +0000

    updated script and added readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# Week 2 Lab – Bash & Git
+
+Adam Malone - x21178691
+
+[devops-lab-2-task](https://github.com/adamm0019/devops-lab-2-task)
+
+---
+
+To run the script.sh run: `bash script.sh`
--- script.sh
+++ script.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-rm -r fol_1 fol_2
+rm -rf fol_1 fol_2
+
 mkdir -p fol_1 fol_2
+
 for i in {1..3}; do
     touch fol_1/1_$i.txt
     touch fol_2/2_$i.txt
 done
+
+chmod 600 fol_*/[1-9]_[13].txt
+
+chmod 777 fol_*/[1-9]_2.txt
+
+echo "Job Completed"
